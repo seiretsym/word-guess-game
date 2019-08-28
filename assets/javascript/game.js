a4e7bce05d40661babb5071107c7f5c9af198188
Kerwin Hy <kerwin.hy@gmail.com>
2019-08-28 12:25:16 -0700

    Add Theme/Hint to index.html layout // declare variables in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// begin work on word guess game
+
+// declare variables
+var textWins = document.getElementById("wins"),
+    textLosses = document.getElementById("losses"),
+    textGuesses = document.getElementById("guesses"),
+    textLettersUsed = document.getElementById("letters-used"),
+    textWord = document.getElementById("word");
+
+// word array
+var words [];
